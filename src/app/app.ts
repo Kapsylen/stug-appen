cd1766db2419de81d2425cffc4f27883a975593f
ssvensson <sebastian.svensson@chas.se>
2025-06-28 22:06:16 +0200

    introduced new utlagg component to be diplsayed when pushing utlagg button

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import {Utlagg} from './components/model/utlagg';
-import {DUMMY_UTLAGG} from '../assets/utlagg_data';
 import {UtlaggbuttonComponent} from './components/buttons/utlaggbutton/utlaggbutton.component';
+import {UtlaggComponent} from './components/utlagg/utlagg.component';
+import {DUMMY_UTLAGG} from '../assets/utlagg_data';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UtlaggbuttonComponent],
+  imports: [HeaderComponent, UtlaggbuttonComponent, UtlaggComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
   isClicked = false;
 
-  utlagg: Utlagg[] = DUMMY_UTLAGG;
+  utlagg: Utlagg[] = DUMMY_UTLAGG
 
   onClickedUtlagg($event: boolean) {
       if($event) {
--- src/app/components/utlagg/utlagg.component.ts
+++ src/app/components/utlagg/utlagg.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CardComponent} from '../../common/card/card.component';
+import {Utlagg} from '../model/utlagg';
 
 @Component({
   selector: 'app-utlagg',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './utlagg.component.html',
   styleUrl: './utlagg.component.css'
 })
 export class UtlaggComponent {
+  @Input({required: true}) utlagg!: Utlagg;
 
 }
